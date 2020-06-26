d73b25f02512dddc6dbf2eee7f0a227dbf208237
Shubham Raut <123shubhamraut@gmail.com>
2020-06-26 19:48:21 +0530

    Basic practice

---
+++ Sprint 3/JS/Basics.js
@@ -0,0 +1,11 @@
+//console.log("Hello World".includes("Hello"));
+//console.log("Hello World".includes("Wo",3));
+// console.log( 'W'.localeCompare('HellP') );
+// console.log(Boolean(" "));
+// var n = undefined;
+// console.log(n + 32);
+// const test = 0;
+// test=8;
+// let x = 99;
+// console.log(++x, x--, x--, x);
+
