a446dfb1912e52d6ccc5ae58a2dc5b44be3a761a
Shubham-raut <123shubhamraut@gmail.com>
2020-09-25 18:19:13 +0530

    destructor and inline func

--- Cpp/Basics/class.cpp
+++ Cpp/Basics/class.cpp
@@ -7,6 +7,37 @@ private:
     int real, imag; //data members
 
 public:
+    //constructor member initializer list
+    Complex() : real(10), imag(20) {}
+
+    // Complex() //constructor (parameterless /default)
+    // {
+    //     cout << "\nParameterless constructor\n";
+    //     real = 1;
+    //     imag = 1;
+    // }
+
+    Complex(int n1, int n2) //constructor (paramaterized )
+    {
+        cout << "\ntwo parameter constructor\n";
+        real = n1;
+        imag = n2;
+    }
+
+    Complex(char a);
+
+    Complex(int real, int imag, char c)
+    {
+        cout << "\ntwo parameter constructor\n";
+        this->real = real;
+        this->imag = imag;
+    }
+
+    ~Complex()
+    {
+        cout << "Destructor Called" << this << endl;
+    }
+
     void accept() //method | member function
     {
         cout << "Enter real Val: ";
@@ -30,12 +61,27 @@ void Complex::hi()
     cout << "Hi\n";
 }
 
+Complex::Complex(char a)
+{
+    cout << "\nOutside Class definition\n";
+    cout << "A= " << a << endl;
+}
+
 int main()
 {
-    Complex obj;
-    obj.accept();
+    Complex obj; //constructor gets called
     obj.display();
-    obj.hi();
-    cout << "Size of class= " << sizeof(obj) << endl; //size of object of class
+    // obj.accept();
+    // obj.display();
+    // obj.hi();
+    // cout << "Size of class= " << sizeof(obj) << endl; //size of object of class
+
+    // Complex obj1(2, 6);
+    // obj1.display();
+
+    // Complex obj2('S');
+
+    // Complex obj3(5, 55, 'A');
+    // obj3.display();
     return 0;
 }
--- Cpp/Basics/functions.cpp
+++ Cpp/Basics/functions.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
 using namespace std;
 
+//inline function
+// it is the request to the compiler-if accepted function
+// call will be replaced with function definition. if not
+// it will be like normal function
+inline int max(int n1, int n2)
+{
+    return (n1 > n2) ? n1 : n2;
+}
+
 //global function
 int sum(int, int);
+
+//default argument
+void print(int a, int b = 0)
+{
+    cout << "A= " << a << " B= " << b << endl;
+}
 
 int main()
 {
@@ -12,6 +27,11 @@ int main()
     cout << "Enter 2nd no. : ";
     cin >> num2;
     cout << "Sum is : " << ::sum(num1, num2) << endl; //:: for global access
+
+    cout << "Maximun of num1 and num2 is: " << max(num1, num2) << endl;
+
+    print(10, 50);
+    print(67);
     return 0;
 }
 
